2d4b357622d7d719a16efe0d454a7314ae6ee9ee
zh <422789400@qq.com>
2016-06-24 18:13:57 +0800

    项目结构

---
+++ src/main/webapp/db/study.sql
@@ -0,0 +1,107 @@
+/*
+SQLyog Ultimate v10.42 
+MySQL - 5.7.10 : Database - study
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`study` /*!40100 DEFAULT CHARACTER SET gbk */;
+
+USE `study`;
+
+/*Table structure for table `tb_department` */
+
+DROP TABLE IF EXISTS `tb_department`;
+
+CREATE TABLE `tb_department` (
+  `id` varchar(50) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+
+/*Table structure for table `tb_resource` */
+
+DROP TABLE IF EXISTS `tb_resource`;
+
+CREATE TABLE `tb_resource` (
+  `id` varchar(50) NOT NULL,
+  `parent_id` varchar(50) DEFAULT NULL,
+  `res_name` varchar(50) DEFAULT NULL,
+  `res_url` varchar(100) DEFAULT NULL,
+  `img` varchar(100) DEFAULT NULL,
+  `remark` varchar(500) DEFAULT NULL,
+  `orderId` int(11) DEFAULT '0',
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_role` */
+
+DROP TABLE IF EXISTS `tb_role`;
+
+CREATE TABLE `tb_role` (
+  `id` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL COMMENT '角色名称',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_role_resouce` */
+
+DROP TABLE IF EXISTS `tb_role_resouce`;
+
+CREATE TABLE `tb_role_resouce` (
+  `id` varchar(50) NOT NULL,
+  `role_id` varchar(50) NOT NULL,
+  `res_id` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_user` */
+
+DROP TABLE IF EXISTS `tb_user`;
+
+CREATE TABLE `tb_user` (
+  `id` varchar(50) NOT NULL,
+  `login_name` varchar(50) DEFAULT NULL COMMENT '登录名',
+  `name` varchar(50) DEFAULT NULL COMMENT '名称',
+  `password` varchar(30) DEFAULT NULL COMMENT '密码',
+  `mobile` varchar(30) DEFAULT NULL COMMENT '手机号',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='state:0未激活 1激活 2停用';
+
+/*Table structure for table `tb_user_department` */
+
+DROP TABLE IF EXISTS `tb_user_department`;
+
+CREATE TABLE `tb_user_department` (
+  `id` varchar(50) NOT NULL,
+  `user_id` varchar(50) DEFAULT NULL,
+  `dep_id` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+
+/*Table structure for table `tb_user_role` */
+
+DROP TABLE IF EXISTS `tb_user_role`;
+
+CREATE TABLE `tb_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

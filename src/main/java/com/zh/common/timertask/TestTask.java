4f6ac6451fb62ef91e726f3456a8c5d6a45d7677
hellovx <422789400@qq.com>
2016-06-29 09:05:52 +0800

    定时器配置

---
+++ src/main/java/com/zh/common/timertask/TestTask.java
@@ -0,0 +1,25 @@
+package com.zh.common.timertask;
+
+/**
+ * 定時器定时器执行定时任务
+ * 
+ * @author vx
+ *
+ */
+public class TestTask {
+
+	public TestTask() {
+		System.out.println("jobs...............");
+	}
+
+	public void testrun1() {
+
+		System.out.println("test run 1...................");
+	}
+	
+	public void testrun2() {
+
+		System.out.println("test run 2...................");
+	}
+
+}
